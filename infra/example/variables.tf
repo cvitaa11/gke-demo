cb1f6a97a91aca2a191f3cabc4fd40aeb36098f0
cvitaa11 <ivancvitkovic98@gmail.com>
2024-10-27 13:06:06 +0100

    feat: add variable types in example dir

--- infra/example/variables.tf
+++ infra/example/variables.tf
@@ -1,27 +1,33 @@
 variable "project_id" {
   description = "GCP project id"
+  type        = string
 }
 
 variable "region" {
   description = "region"
   default     = "us-east1" # Free tier region
+  type        = string
 }
 
 variable "node_locations" {
   description = "Availability zone of the GKE nodes"
+  type        = list(string)
 }
 
 variable "node_ip_range" {
   default     = "10.0.0.0/16"
   description = "IP address range of GKE nodes"
+  type        = string
 }
 
 variable "pod_ip_range" {
   default     = "10.1.0.0/16"
   description = "IP address range of k8s pods"
+  type        = string
 }
 
 variable "service_ip_range" {
   default     = "10.2.0.0/16"
   description = "IP address range of k8s services"
+  type        = string
 }
